3225d957a5260414cd9b9bea861e9b3264c4fefd
taep96 <64481039+taep96@users.noreply.github.com>
2022-02-11 23:11:37 +0100

    customization

--- config.h
+++ config.h
@@ -2,24 +2,24 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt                = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*                         fg         bg       */
+	[SchemeNorm]          = { "#d5c4a1", "#282828" },
+	[SchemeSel]           = { "#d5c4a1", "#4c443e" },
+	[SchemeSelHighlight]  = { "#98971a", "#282828" },
+	[SchemeNormHighlight] = { "#689d6a", "#282828" },
+	[SchemeOut]           = { "#d5c4a1", "#458588" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines          = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
+static unsigned int lineheight     = 22;
 static unsigned int min_lineheight = 8;
 
 /*
